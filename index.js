c3716e5ba865c911ce92e3353e801cb107482237
AnkushShenoy <ankush.shenoy@niveussolutions.com>
2024-11-15 14:36:21 +0530

    feat: createFile function added which uses promise

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const messagePromise = new Promise((resolve, _) => {
   resolve("This is a new message");
 });
@@ -9,3 +11,20 @@ messagePromise
   .catch((err) => {
     console.error(err);
   });
+
+const createFile = (filename, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, (err) => {
+      if (err) reject(err);
+      resolve("File create successful!");
+    });
+  });
+};
+
+(async () => {
+  const response = await createFile(
+    "index.html",
+    `<html><body><h1>This is html</h1></body></html>`
+  );
+  console.log(response);
+})();
